a2a22667f933c6fc48cf633a07fbfb737514499d
lgkimjy <lgkimjy@naver.com>
2021-06-17 13:55:36 +0900

    bashrc changed and matplotlib added in Dockerfile

--- Dockerfiles/ros-melodic-mac/docker_run.sh
+++ Dockerfiles/ros-melodic-mac/docker_run.sh
@@ -7,4 +7,4 @@ docker run -it -d \
     --name simulation \
     -e DISPLAY=$ip:0 \
     -v $HOME/Desktop/docker_share:/home/junyoung/catkin_ws/src \
-    lgkimjy/ubuntu:arm64
+    lgkimjy/ubuntu:mac
