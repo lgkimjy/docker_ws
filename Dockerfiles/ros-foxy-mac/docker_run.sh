d45922d72dd4bc30f08c54abfa16f33c90dc3ddb
lgkimjy <lgkimjy@naver.com>
2021-10-05 16:42:32 +0900

    updates ros-foxy-mac

--- Dockerfiles/ros-foxy-mac/docker_run.sh
+++ Dockerfiles/ros-foxy-mac/docker_run.sh
@@ -4,7 +4,7 @@ ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost +$ip
 
 docker run -it -d \
-    --name ros2 \
+    --name ros2_lab \
     -e DISPLAY=$ip:0 \
     -v $HOME/Desktop/docker_share:/home/junyoung/docker_share \
     lgkimjy/ubuntu:mac-foxy
